f58c5847a096f7b8c780bd3c0e147fd8498a06a1
vismay12195 <130983117+vismay12195@users.noreply.github.com>
2023-10-24 16:41:14 +0530

    Add files via upload
    These files cover most of the concepts of JavaScript from Basic to Advanced level.

---
+++ Advanced JS/Mod1_2.js
@@ -0,0 +1,17 @@
+//  Q.2 What will be the result for these expressions?
+// 1. 5 > 4
+// 2. "apple" > "pineapple"
+// 3. "2" > "12"
+// 4. undefined == null
+// 5. undefined === null
+// 6. null == "\n0\n"
+// 7. 7. null === +"\n0\n" 
+
+
+console.log(5 > 4); // output: true
+console.log("apple" > "pineapple"); // output: false
+console.log("2" > "12"); // output: true
+console.log(undefined == null); // output: true
+console.log(undefined === null); // output: false
+console.log(null == "\n0\n"); // output: false
+console.log(null === +"\n0\n"); // output: false
---
+++ Advanced JS/Mod2_1.js
@@ -0,0 +1,28 @@
+// Q.1 Write the code, one line for each action:
+// a) Create an empty object user.
+// b) Add the property name with the value John.
+// c) Add the property surname with the value Smith.
+// d) Change the value of the name to Pete.
+// e) Remove the property name from the object.
+
+// a) Create an empty object user.
+// const user = {};
+// OR using the object constructor
+// const user2 = new Object();
+
+// b) Add the property name with the value John.
+// const user = {
+//     "name": "John"
+// };
+
+// c) Add the property surname with the value Smith
+// const user = {
+//     "name": "John",
+//     "surname": "Smith"
+// };
+
+// d) Change the value of the name to Pete.
+// user.name = "Pete";
+
+// e) Remove the property name from the object.
+// delete user.name;
